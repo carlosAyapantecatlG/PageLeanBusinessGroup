31800675332d9ca8c00aaa08fc93c54d5d6503ce
carlosAyapantecatlG <carlos.ayapantecatl@gmail.com>
2020-10-29 11:56:55 -0600

    commit

--- src/conexionesdb/conexion.js
+++ src/conexionesdb/conexion.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/pageLBG';
+//mongodb://myUser:12345678@x.x.x.x:xxxxx/myDatabase
+const url = 'mongodb://192.168.192.131:27017/pageLBG';
+const url3 = 'mongodb://admin:Ah$69M0)dn#23@35.166.9.102:27017/pageLBG?authSource=admin';
+const url2= 'mongodb://localhost:27017/pageLBG';
 
 const db = mongoose.connection;
-mongoose.connect(url, { 
+
+mongoose.connect(url3, { 
     useNewUrlParser : true,
     useUnifiedTopology : true
 }).catch(err => console.log(err));
 
 db.on('open', _=> { 
-    console.log('Database is connected to:' , url);
+    console.log('Database is connected to:' , url3);
 });
 
 db.on('err', err => { 
